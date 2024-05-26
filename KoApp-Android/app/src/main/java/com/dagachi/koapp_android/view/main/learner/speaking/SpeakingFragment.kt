0e0f63bab2a82c74156201601a46266e223a2a8d
Yoonji Lee <yooonji08@naver.com>
2024-05-26 11:52:09 +0900

    ✨ Feat: 말하기 연습 화면 UI 구현 #1

--- KoApp-Android/app/src/main/java/com/dagachi/koapp_android/view/main/learner/speaking/SpeakingFragment.kt
+++ KoApp-Android/app/src/main/java/com/dagachi/koapp_android/view/main/learner/speaking/SpeakingFragment.kt
@@ -1,5 +1,7 @@
 package com.dagachi.koapp_android.view.main.learner.speaking
 
+import androidx.core.content.ContextCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.dagachi.koapp_android.R
 import com.dagachi.koapp_android.databinding.FragmentSpeakingBinding
 import com.dagachi.koapp_android.view.base.BaseFragment
@@ -7,6 +9,15 @@ import com.dagachi.koapp_android.view.base.BaseFragment
 /* 학습자의 말하기 연습 화면 */
 class SpeakingFragment: BaseFragment<FragmentSpeakingBinding>(FragmentSpeakingBinding::inflate) {
     override fun initViewCreated() {
+        mainActivity!!.window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.Main_10) // status bar 색상
+        WindowInsetsControllerCompat(mainActivity!!.window, mainActivity!!.window.decorView).isAppearanceLightStatusBars = true
+
+        mainActivity?.hideLearnerBottomNav(false)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mainActivity!!.window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.Gray_100) // status bar 색상
     }
 
     override fun initAfterBinding() {
