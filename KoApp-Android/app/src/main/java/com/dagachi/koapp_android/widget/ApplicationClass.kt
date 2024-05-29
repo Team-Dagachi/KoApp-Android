90ce7cffebd500c3388c4b9e094e5601529a40d0
Yoonji Lee <yooonji08@naver.com>
2024-05-29 17:13:38 +0900

    ✨ Feat: applicationclass 파일 추가 #2

---
+++ KoApp-Android/app/src/main/java/com/dagachi/koapp_android/widget/ApplicationClass.kt
@@ -0,0 +1,19 @@
+package com.dagachi.koapp_android.widget
+
+import android.app.Application
+import android.content.Context
+
+/* 공통적으로 사용하는 데이터를 관리하는 파일 */
+class ApplicationClass: Application() {
+    init {
+        instance = this
+    }
+    companion object {
+        lateinit var instance: ApplicationClass
+
+        // context 가져오기
+        fun applicationContext(): Context {
+            return instance.applicationContext
+        }
+    }
+}
