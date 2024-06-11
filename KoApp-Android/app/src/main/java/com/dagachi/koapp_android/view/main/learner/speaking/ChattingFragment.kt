2c20ed93536c02152a9b9c040b4e29acdb14f1eb
growingpsy <psy915@swu.ac.kr>
2024-06-12 03:22:05 +0900

    ✨ Feat: 마이크 버튼 구현 및 STT 구현

--- KoApp-Android/app/src/main/java/com/dagachi/koapp_android/view/main/learner/speaking/ChattingFragment.kt
+++ KoApp-Android/app/src/main/java/com/dagachi/koapp_android/view/main/learner/speaking/ChattingFragment.kt
@@ -1,5 +1,12 @@
 package com.dagachi.koapp_android.view.main.learner.speaking
 
+import android.content.Intent
+import android.os.Bundle
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.view.View
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -12,6 +19,7 @@ import com.dagachi.koapp_android.data.remote.model.ChatRole
 import com.dagachi.koapp_android.databinding.FragmentChattingBinding
 import com.dagachi.koapp_android.view.base.BaseFragment
 import com.dagachi.koapp_android.view.main.learner.speaking.adapter.ChattingAdapter
+import com.dagachi.koapp_android.view.main.learner.speaking.listener.SpeakingRecognitionListener
 import com.dagachi.koapp_android.viewmodel.main.learner.speaking.ChattingViewModel
 import com.google.ai.client.generativeai.GenerativeModel
 import com.google.ai.client.generativeai.type.BlockThreshold
@@ -21,16 +29,19 @@ import com.google.ai.client.generativeai.type.content
 import com.google.ai.client.generativeai.type.generationConfig
 
 /* Gemini 챗봇과의 주제별 말하기연습 스피킹 화면 */
-class ChattingFragment: BaseFragment<FragmentChattingBinding>(FragmentChattingBinding::inflate) {
+class ChattingFragment : BaseFragment<FragmentChattingBinding>(FragmentChattingBinding::inflate) {
     private lateinit var viewModel: ChattingViewModel // 뷰모델
+    private lateinit var speechRecognizer: SpeechRecognizer // STT 를 위한 sdk 객체
     private var chattingAdapter = ChattingAdapter() // 채팅 어댑터
     private var messageList = mutableListOf<ChatMessage>() // 채팅 리스트
     private var systemInstruction: String? = null // 프롬프트
     private var initMessage: String? = null // 초기 메시지
+    private var isRecording = false
 
     override fun initViewCreated() {
         mainActivity!!.hideLearnerBottomNav(true)
-        mainActivity!!.window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.Gray_100) // status bar 색상
+        mainActivity!!.window.statusBarColor =
+            ContextCompat.getColor(requireContext(), R.color.Gray_100) // status bar 색상
 
         val args: ChattingFragmentArgs by navArgs()
         when (args.situationKorTitle) {
@@ -39,6 +50,7 @@ class ChattingFragment: BaseFragment<FragmentChattingBinding>(FragmentChattingBi
                 systemInstruction = getString(R.string.prompt_school_teacher)
                 initMessage = "요새 학교 생활은 어때요?"
             }
+
             getString(R.string.speaking_school_friend) -> {
                 systemInstruction = getString(R.string.prompt_school_friend)
                 initMessage = "내일 챙겨와야 하는 준비물이 뭐야?"
@@ -48,10 +60,12 @@ class ChattingFragment: BaseFragment<FragmentChattingBinding>(FragmentChattingBi
                 systemInstruction = getString(R.string.prompt_media_and_content_kpop)
                 initMessage = "혹시 좋아하는 K-POP 가수 있어요?"
             }
+
             getString(R.string.speaking_media_and_content_kdrama) -> {
                 systemInstruction = getString(R.string.prompt_media_and_content_kdrama)
                 initMessage = "혹시 좋아하는 한국 드라마 있어요?"
             }
+
             getString(R.string.speaking_media_and_content_game) -> {
                 systemInstruction = getString(R.string.prompt_media_and_content_game)
                 initMessage = "혹시 좋아하는 게임 있어요?"
@@ -98,26 +112,87 @@ class ChattingFragment: BaseFragment<FragmentChattingBinding>(FragmentChattingBi
         binding.rvChatting.adapter = chattingAdapter // 어댑터 연결
         chattingAdapter.setMessage(messageList) // 초기 질문 넣기
 
-        sendUserMessage() // 메시지 전송
+        binding.iBtnFragmentChattingMic.setOnClickListener {
+            startListening(getRecognitionListener())
+        }
+
         observeMessage() // 메시지 받기
     }
 
-    // 메시지 보내기
-    private fun sendUserMessage() {
-        binding.btnChattingSend.setOnClickListener {
-            val userMessage = binding.edtChatting.text.toString().trim()
-            if (userMessage.isNotEmpty()) {
-                viewModel.geminiChat(userMessage)
-                messageList.add(ChatMessage(ChatRole.USER, userMessage)) // 사용자 메시지 추가
-                chattingAdapter.setMessage(messageList)
-                scrollToBottom()
-
-                // 메시지 입력창에 입력되었던 텍스트 삭제
-                binding.edtChatting.text = null
-                mainActivity?.getHideKeyboard(binding.root) // 키보드 숨기기
+    private fun startListening(listener: RecognitionListener) {
+        if (!isRecording) {
+            val recordingIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+                putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, requireContext().packageName)
+                putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR")
+            }
+            speechRecognizer = SpeechRecognizer.createSpeechRecognizer(requireContext()).apply {
+                setRecognitionListener(listener)
+                startListening(recordingIntent)
             }
         }
     }
+
+    private fun showRecognizerErrorToast(recognizerError: Int) {
+        val errorMessage = when (recognizerError) {
+            SpeechRecognizer.ERROR_AUDIO -> "오디오 레코딩 오류"
+            SpeechRecognizer.ERROR_CLIENT -> "디바이스 관련 오류"
+            SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS -> "권한 오류"
+            SpeechRecognizer.ERROR_NETWORK -> "네트워크 관련 오류"
+            SpeechRecognizer.ERROR_NETWORK_TIMEOUT -> "네트워크 응답 시간 초과"
+            SpeechRecognizer.ERROR_NO_MATCH -> "결과 불러오기 실패"
+            SpeechRecognizer.ERROR_SPEECH_TIMEOUT -> "녹음에 실패 하였습니다."
+            else -> "잠시 후 다시 시도 해주세요"
+        }
+
+        setButtonNormalState()
+        showToast(errorMessage, Toast.LENGTH_LONG)
+        isRecording = false
+    }
+
+    private fun setButtonNormalState() {
+        binding.iBtnFragmentChattingMic.setImageResource(R.drawable.ic_mic_36)
+        binding.tvFragmentChattingRecTooltip.visibility = View.INVISIBLE
+    }
+
+    private fun getRecognitionListener(): RecognitionListener =
+        binding.tvFragmentChattingRecTooltip.run {
+            val button = binding.iBtnFragmentChattingMic
+            SpeakingRecognitionListener(
+                {
+                    button.setImageResource(R.drawable.ic_rec_36)
+                    text = getString(R.string.speaking_recording_tooltip)
+                    visibility = View.VISIBLE
+                },
+                {
+                    button.setImageResource(R.drawable.ic_loading_36)
+                    text = getString(R.string.speaking_recording_analysis)
+                    visibility = View.VISIBLE
+                },
+                {
+                    /**
+                     * STT 결과 전송 및 UI 처리
+                     */
+                    runCatching {
+                        val userMessage =
+                            it?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+                                ?.joinToString("") ?: ""
+                        if (userMessage.isNotEmpty()) {
+                            viewModel.geminiChat(userMessage)
+                            messageList.add(ChatMessage(ChatRole.USER, userMessage))
+                            chattingAdapter.setMessage(messageList)
+                            scrollToBottom()
+                        }
+                        isRecording = false
+                        setButtonNormalState()
+                    }.onFailure {
+                        showRecognizerErrorToast(-1) // 결과 전송중 오류 이므로 -1 로 보냅니다.
+                    }
+                },
+                {
+                    showRecognizerErrorToast(it)
+                },
+            )
+        }
 
     // 메시지 받기
     private fun observeMessage() {
